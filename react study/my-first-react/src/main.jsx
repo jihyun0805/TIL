b86a9e5ea34148460a5b03cb4c8a65354fdd0f95
jihyun0805 <cjhapple0805@gmail.com>
2026-01-11 16:14:49 +0900

    2026.01.10

--- react study/my-first-react/src/main.jsx
+++ react study/my-first-react/src/main.jsx
@@ -1,24 +1,21 @@
-import React,{ Suspense } from "react";
 import { createRoot } from "react-dom/client";
-// import Greeting from "./Greeting";
-
-// const LazyGreeting = React.lazy(() => import('./Greeting'));
-
-const LazyGreeting = React.lazy(() => 
-  new Promise((resolve) => {
-    setTimeout(() => resolve(import('./Greeting')), 2000);
-  })
-);
+import { useRef } from "react";
 
 function App() {
+  const a = useRef();
+
+  const focusInput = () => {
+    a.current.focus();
+  }
+
   return (
-    <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <LazyGreeting />
-      </Suspense>
-    </div>
+    <>
+      <input type="text" ref={a} />
+      <button onClick={focusInput}>Input box에 주기</button>
+    </>
   )
 }
+
 
 createRoot(document.getElementById('root')).render(
   <App />
