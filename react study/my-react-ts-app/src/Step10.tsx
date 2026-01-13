eec727e5cb98bf7d41f40c970f5061dc9f130cae
JiHyun <cjhapple0805@gmail.com>
2026-01-13 17:36:06 +0900

    2026.01.13

---
+++ react study/my-react-ts-app/src/Step10.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+type User = {
+    id: number;
+    name: string;
+}
+
+function Step10() {
+    const [ users, setUsers] = useState<User[]>([]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setUsers([
+                {id: 1, name:"홍길동"},
+                {id: 2, name:"이몽룡"},
+                {id: 3, name:"성춘향"},
+            ]);
+        }, 1000);
+    }, []);
+}
+
+return (
+    <div>
+        <h3>회원 목록</h3>
+        {useFormState.length === 0? (
+            
+        )}
+    </div>
+)
